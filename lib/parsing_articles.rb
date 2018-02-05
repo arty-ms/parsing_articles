4d09b1d5c234689eadb86501343a6dbc79e2a38c
morozartem9 <morozartem9@gmail.com>
2018-02-05 14:45:53 +0300

    forgot add

--- lib/parsing_articles.rb
+++ lib/parsing_articles.rb
@@ -1,5 +1,61 @@
 require "parsing_articles/version"
-
+require 'rss'
+require 'open-uri'
+require 'nokogiri' 
+require 'koala' 
 module ParsingArticles
-  # Your code goes here...
+  class GetArticles
+    def initialize(url)
+      @url = url.to_s
+      @articles_results = []
+    end
+
+    def self.config_facebook(app_id = nil, app_secret = nil)
+      @APP_ID = app_id
+      @APP_KEY = app_secret
+    end
+
+    def get_array
+      facebook_or_rss
+    end
+
+    private
+    def facebook_or_rss
+      /https:\/\/www.facebook.com/.match(@url) ? facebook_parser : rss_parser 
+    end
+
+    def facebook_parser
+      graph = Koala::Facebook::API.new(GetArticles.get_app_access_token)
+      get_facebook_array(graph)
+    end
+
+    def self.get_app_access_token
+      @oauth = Koala::Facebook::OAuth.new(@APP_ID ||= '1977681792501372', @APP_KEY ||='920ff6a0a18835cb7b2fef5cda8cd491')
+      @oauth.get_app_access_token
+    end
+    
+    def get_facebook_array(graph)
+      @articles_results = graph.get_connection(facebook_get_page_id, 'posts', {
+      fields: ['title', 'link', 'message', 'created_time']})
+    end
+
+    def facebook_get_page_id
+      page = Nokogiri::HTML(open(@url))
+      page_id = /([\d]){15}/.match((page.css('head')).to_s)
+    end
+
+    def rss_parser()
+      rss = RSS::Parser.parse(@url, false).items
+      get_rss_array(rss) do |result|
+        result = { 'title'=>result.title, 'created_time'=>result.pubDate, 'link'=>result.link, 'message'=>result.description.gsub(/<\/?[^>]*>/, "") }
+        @articles_results.push(result)
+      end   
+    end
+
+    def get_rss_array(rss, &block)
+      rss.each &block
+      @articles_results
+    end
+    
+  end
 end
--- parsing_articles.gemspec
+++ parsing_articles.gemspec
@@ -3,15 +3,17 @@ lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "parsing_articles/version"
 
+
+
 Gem::Specification.new do |spec|
   spec.name          = "parsing_articles"
   spec.version       = ParsingArticles::VERSION
   spec.authors       = ["morozartem9"]
   spec.email         = ["morozartem9@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "parsing articles"
+  spec.description   = " parsing articles from rss or facebook"
+  spec.homepage      = "TODO: https://github.com/morozartem9/parsing_articles"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
@@ -32,4 +34,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_dependency "nokogiri", ">=1.8.2"
+  spec.add_dependency "activesupport"
+  spec.add_dependency "koala"
+
 end
