480edcdadef043068b05b023f31414ba12595d21
morozartem9 <morozartem9@gmail.com>
2018-02-07 15:23:10 +0300

    Added getting images from articles, using Enumerable#map instead of #get_rss_array method

--- lib/parsing_articles.rb
+++ lib/parsing_articles.rb
@@ -29,27 +29,25 @@ module ParsingArticles
     end
 
     def self.get_app_access_token
-      @oauth = Koala::Facebook::OAuth.new(@APP_ID || = '1977681792501372', @APP_KEY || ='920ff6a0a18835cb7b2fef5cda8cd491')
+      @oauth = Koala::Facebook::OAuth.new(@APP_ID ||= '1977681792501372', @APP_KEY ||='920ff6a0a18835cb7b2fef5cda8cd491')
       @oauth.get_app_access_token
     end
 
     def get_facebook_array(graph)
       @articles_results = graph.get_connection(graph.get_object('', id: @url)['id'], 'posts', {
-      fields: ['title', 'link', 'message', 'created_time']})
+      fields: ['title', 'link', 'message', 'picture', 'created_time']})
     end
 
     def rss_parser()
       rss = RSS::Parser.parse(@url, false).items
-      get_rss_array(rss) do |result|
-        result = { 'title' => result.title, 'created_time' => result.pubDate, 'link' => result.link, 'message' => result.description.gsub(/<\/?[^>]*>/, "")}
+      rss.map do |result|
+        result = { 'title' => result.title, 
+        'created_time' => result.pubDate, 
+        'link' => result.link, 
+        'picture' => result.description[/img.*?src="(.*?)"/i,1], 
+        'message' => result.description.gsub(/<\/?[^>]*>/, "")}
         @articles_results.push(result)
       end   
-    end
-
-    def get_rss_array(rss, &block)
-      rss.each &block
-      @articles_results
-    end
-    
+    end 
   end
 end
