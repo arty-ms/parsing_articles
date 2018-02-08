78a3af4287f52793860db346d13d9fa1c5b16d55
morozartem9 <morozartem9@gmail.com>
2018-02-08 03:41:40 +0300

    Finished tests

--- spec/parsing_articles_spec.rb
+++ spec/parsing_articles_spec.rb
@@ -1,21 +1,36 @@
 
 RSpec.describe ParsingArticles do
-  describe "Testing Facebook connection and parsing" do
-    it "rhzzzz" do
-    VCR.use_cassette("vcr_facebook") do
+  describe "if facebook url was given to Parse" do
+    it "if #get_array returns array" do
+      VCR.use_cassette("vcr_facebook") do
         @facebook = ParsingArticles::Parse.new('https://www.facebook.com/jonloomerdigital/').get_array
-        
         expect(@facebook).to be_kind_of(Array)
-          end
-             
+      end           
     end
   end
 
-  describe "if nothing given" do
+  describe "Testing only Facebook class" do
+    it "if #get_array returns array" do
+      VCR.use_cassette("vcr_facebook") do
+        @facebook = ParsingArticles::Facebook.new('https://www.facebook.com/jonloomerdigital/').send(:parse)
+        expect(@facebook).to be_kind_of(Array)
+      end
+    end
+  end
+  describe "if nothing given to Parse" do
     it "rss_parser method raise an error" do
       expect{ParsingArticles::Parse.new('').get_array}.to raise_error(NoMethodError)
     end
   end
 
+  describe "if rss url was given to Parse" do
+    it "if #get_array returns array" do
+      VCR.use_cassette("vcr_rss") do
+        @rss = ParsingArticles::Rss.new('https://www.onliner.by/feed').send(:parse)
+        expect(@rss).to be_kind_of(Array)
+
+      end
+    end
+  end
   
 end
